19364a888b64007ad6e52fb84fc0a657293bfa52
Denisse Herrera <denher11@gmail.com>
2025-01-20 12:39:02 -0800

    photo updates

--- assets/assets.js
+++ assets/assets.js
@@ -1,4 +1,5 @@
-import user_image from './portrait.png';
+import user_image from './portrait.jpg';
+import user_cartoon from './cartoon.jpg';
 import code_icon from './code-icon.png';
 import code_icon_dark from './code-icon-dark.png';
 import edu_icon from './edu-icon.png';
@@ -40,6 +41,7 @@ import right_arrow_bold_dark from './right-arrow-bold-dark.png';
 
 export const assets = {
     user_image,
+    user_cartoon,
     code_icon,
     code_icon_dark,
     edu_icon,
